74a6e6c680c9800d46ede16abf2158cd12ce6086
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2026-05-24 16:17:21 -0400

    refactor: migrate model classes to Java 17 record

--- producer-service/src/main/java/org/kiwiproject/samples/activemq/producer/model/ProduceResponse.java
+++ producer-service/src/main/java/org/kiwiproject/samples/activemq/producer/model/ProduceResponse.java
@@ -1,11 +1,9 @@
 package org.kiwiproject.samples.activemq.producer.model;
 
-import lombok.Value;
-
-@Value
-public class ProduceResponse {
-    String serviceName;
-    int sent;
-    String destination;
-    boolean sentToAllEventsQueue;
+public record ProduceResponse(
+        String serviceName,
+        int sent,
+        String destination,
+        boolean sentToAllEventsQueue
+) {
 }
